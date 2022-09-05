6d36f82d0e32849f9b68996a0f256cc4900b3d20
davidnene <davienesh4@gmail.com>
2022-09-05 14:34:08 +0300

    Add methods

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,15 @@
 class Concert
-    
+    attr_accessor :band
+    attr_reader :venue, :date
+    @@all = []
+    def initialize(date, band, venue)
+        @date = date
+        @band = band
+        @venue = venue
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
