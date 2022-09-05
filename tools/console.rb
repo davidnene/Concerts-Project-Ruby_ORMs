ec8296ff673aab538de3475f358f4307165732fc
davidnene <davienesh4@gmail.com>
2022-09-05 14:49:16 +0300

    Fix instances

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,15 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+b1 = Band.new("Burukyln boyz", "Buruburu")
+b2 = Band.new("Barida", "Kisumu")
+
+v1 = Venue.new("Soweto VR", "Nairobi")
+v2 = Venue.new("Francais", "Nairobi")
+
+c1 = Concert.new("05/sept/22",b1, v1)
+c2 = Concert.new("12/sept/22", b2, v2)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
